648d9b895d289e2d772bc9aa70e4a6bf37e24b63
neilprado <neiljohn17@gmail.com>
2017-06-03 16:26:31 -0300

    Delete ex2.2.c

--- ex2.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int num, total;
-    printf("Informe o valor \n");
-    scanf("%d", &num);
-    total = num * 3;
-    printf("O valor e: %d", total);
-    return 0;
-}
